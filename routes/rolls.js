7c7adddb15e710f5cad94e3f5c587ca04ccf639f
pedrofranca <pedro.caputo@live.com>
2019-01-02 20:09:28 -0200

    Mudando

--- routes/rolls.js
+++ routes/rolls.js
@@ -1,7 +1,7 @@
 const rotas = require('express').Router();
 
 rotas.get('/singlepull', (req, res) => {
-  res.status(200).json({message : 'Connected!'});
+  res.status(200).json({message : 'Connected!!'});
 });
 
 rotas.get('/tenplusone', (req, res) => {
